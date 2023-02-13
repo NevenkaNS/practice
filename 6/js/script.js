a066f1718129ce9157e059e86c2761366afcf22a
NevenkaNS <104096559+NevenkaNS@users.noreply.github.com>
2023-02-13 18:38:49 +0100

    Add files via upload

---
+++ 6/js/script.js
@@ -0,0 +1,14 @@
+function displayMeny() {
+ document.getElementById ("menu-small").style.display = "none";
+ document.getElementById ("close-menu").style.display = "inline-block";
+ document.getElementById ("nav-bar").style.display = "inline";
+
+}
+function closeMenu (){
+    document.getElementById ("close-menu").style.display = "none";
+    document.getElementById ("nav-bar").style.display = "none";
+    document.getElementById ("menu-small").style.display = "block";
+   
+}
+
+
